629f91fe5431adce9f820836be82e85fdd1be4e8
bagpipes <dylanswafflez@gmail.com>
2021-01-16 23:35:55 -0800

    mongodb

--- server.js
+++ server.js
@@ -5,9 +5,20 @@ var request = require('request');
 var app = express();
 app.use(cors());
 
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/";
+
 app.get('/', function(req, res){
-    console.log("hello")
-    res.send("hello")
+	MongoClient.connect(url, function(err, db) {
+	  if (err) throw err;
+	  var dbo = db.db("mydb");
+	  dbo.collection("locations").findOne({}, function(err, result) {
+	    if (err) throw err;
+	    console.log(result.name);
+	    res.send(result);
+	    db.close();
+	  });
+	});
 });
 
 app.listen(5000);
